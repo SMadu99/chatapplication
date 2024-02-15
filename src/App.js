4a89ae145e1084d177273e82d7452693e52cb242
Shanilka Gurusinghe <128635738+SMadu99@users.noreply.github.com>
2024-02-15 21:06:14 +0530

    Add pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './style.scss';
 import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div >
-      <Register/>
+      <Login/>
     </div>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import Addimg from "../images/addimg.png"
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span  className="logo">SM Chat</span>
+        <span className="title">Login</span>
+        <form>
+          
+          <input type="email" placeholder="Email"/>
+          <input type="password" placeholder="Password"/>
+          
+          <button>Sign In</button>
+        </form>
+        <p>You don't have an account? Register</p>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
